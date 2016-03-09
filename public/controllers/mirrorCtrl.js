9755ca6239454602f23cbbc74f565e6314ea54fb
Amy Lee <amyleeannebennett@gmail.com>
2016-03-09 10:09:52 -0600

    changed all api calls to server with api routes

--- public/controllers/mirrorCtrl.js
+++ public/controllers/mirrorCtrl.js
@@ -2,65 +2,36 @@
 var myApp = angular.module("myApp");
 
 myApp.controller('MirrorController', ['$scope', '$http','$location','$routeParams', '$q', '$window', function($scope, $http,$location, $routeParams, $q, $window){
-  console.log('Mirror Controller Initialized...');
 
   const now = moment(new Date());
-  const apikey = '6b33eb7b78a214401d2766c100aeae6a:5:74631439';
-  const newsUrl = `http://api.nytimes.com/svc/topstories/v1/home.json?api-key=${apikey}`;
-  const quotesUrl = 'http://quotes.rest/qod.json';
   const googleKey = 'AIzaSyBWxxUFMPhxC64LOxG-G_mD-oc0siASEY8'
 
   $scope.time = now.format('LT');
   $scope.date = moment().format('MMMM Do YYYY')
 
   const quoteApi = () => {
-    var deferred = $q.defer();
-
-    $http.get(quotesUrl)
-    .then(function (quote) {
-      console.log("data", quote.data.contents.quotes[0]);
-      deferred.resolve(quote);
-      $scope.quote = quote.data.contents.quotes[0].quote
-      $scope.author = quote.data.contents.quotes[0].author
-    })
-
-    .catch(function () {
-      deferred.reject();
+    $http.get('/api/quote').success((response) => {
+      console.log("quote response", response);
+      $scope.quote = response.contents.quotes[0].quote;
+      $scope.author = response.contents.quotes[0].author;
     });
-
-    // return promise object
-    return deferred.promise;
   }
-quoteApi();
+  quoteApi();
 
-$scope.map = { center: { latitude: 36.174465, longitude: -86.767960 }, zoom: 8 };
+  // $scope.map = { center: { latitude: 36.174465, longitude: -86.767960 }, zoom: 8 };
 
   const newsApi = () => {
-      var deferred = $q.defer();
-
-      $http.get(newsUrl)
-      .then(function (news) {
-        console.log("news", news)
-        deferred.resolve(news);
-        $scope.news = news.data.results;
-      })
-      .catch(function () {
-        deferred.reject();
-      });
-
-      // return promise object
-      return deferred.promise;
-    }
-  newsApi();
+    $http.get('/api/news').success((response) => {
+      $scope.news = response.results;
+    });
+  }
+  newsApi()
 
   const weatherApi = () => {
     $http.get('/api/weather').success((response) => {
       $scope.weather = response.currently;
       $scope.temperature = parseInt(response.currently.temperature)
       $scope.fiveDay = response.daily.data
-      console.log("fi", $scope.fiveDay);
-      console.log("weather", response);
-      console.log("weather --", response.currently);
     });
   }
 
--- server.js
+++ server.js
@@ -3,35 +3,41 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const request = require('request');
+var apicache = require('apicache').options({ debug: true }).middleware;
 const weatherUrl = 'https://api.forecast.io/forecast/88eb88be23406cc2c563c798d72aa649/36.1820800,-86.7278270'
-const googleMapsUrl = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=700+Wilsonwood+Pl.+Nashville,+TN+37206&destinations=500+Interstate+Blvd+S,+Nashville,+TN+37210&mode=driving&language=en&key=AIzaSyBWxxUFMPhxC64LOxG-G_mD-oc0siASEY8'
+const apikey = '6b33eb7b78a214401d2766c100aeae6a:5:74631439';
+const newsUrl = `http://api.nytimes.com/svc/topstories/v1/home.json?api-key=${apikey}`;
+const quoteUrl = 'http://quotes.rest/qod.json';
+
 
 app.set('view engine', 'jade')
-// app.setHeader('Access-Control-Allow-Origin', '*');
-
 app.use(express.static('public'))
-
 app.get('/');
 
-app.get('/api/weather', (req, res) => {
+app.get('/api/weather', apicache('10 minutes'), (req, res) => {
   request.get(weatherUrl, (err, response, body) => {
     if (err) throw err;
     res.header('Access-Controll-Allow-Origin', '*');
     const weather = JSON.parse(body);
-    console.log("weather",weather);
     res.send(weather)
-
   })
 })
 
-app.get('/api/maps', (req, res) => {
-  request.get(googleMapsUrl, (err, response, body) => {
+app.get('/api/news', apicache('1 hour'), (req, res) => {
+  request.get(newsUrl, (err, response, body) => {
     if (err) throw err;
-    res.header('Access-Controll-Allow-Origin', '*');
-    const map = JSON.parse(body);
-    console.log("map",map);
-    res.send(map)
+    const news = JSON.parse(body);
+    console.log("news",news);
+    res.send(news)
+  })
+})
 
+app.get('/api/quote', apicache('12 hours'), (req, res) => {
+  request.get(quoteUrl, (err, response, body) => {
+    if (err) throw err;
+    const quote = JSON.parse(body);
+    console.log("quote",quote);
+    res.send(quote)
   })
 })
 
